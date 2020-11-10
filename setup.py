d407a1a677cec9d0bf7954f5cb3315e023b8de91
Emanuel Forlín <emaforlin@gmail.com>
2020-11-10 13:58:48 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   url = 'https://github.com/emaaForlin/TelegramBotInterface',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/emaaForlin/TelegramBotInterface/archive/0.4.tar.gz',    # I explain this later on
   keywords = ['API', 'BOT', 'TELEGRAM', 'INTERFACE'],   # Keywords that define your package best
-  install_requires=['requests', 'json'],            # I get to this in a second
+  install_requires=['requests'],            # I get to this in a second
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
