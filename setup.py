6f00e0f0f4b4278359a8591d92c198a4d8086b70
Emanuel Forlín <emaforlin@gmail.com>
2020-11-10 11:53:49 -0300

    some changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Emanuel Forlin',                   # Type in your name
   author_email = 'emaforlin@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/emaaForlin/TelegramBotInterface',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/emaaForlin/TelegramBotInterface/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['API', 'BOT', 'TELEGRAM', 'INTERFACE'],   # Keywords that define your package best
   install_requires=['requests', 'json'],            # I get to this in a second
   classifiers=[
